2784b797fd7cf8b82e599245509cfa14dd865ad0
Charles Birk <charles@cryptolab.net>
2018-08-31 21:37:32 -0400

    Mattermost plugin to test slack attachment parsing

---
+++ plugin.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/subtle"
+	"encoding/json"
+	"net/http"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+)
+
+type Plugin struct {
+	plugin.MattermostPlugin
+	Enabled bool
+}
+
+// OnActivate is invoked when the plugin is activated.
+func (p *Plugin) OnActivate() error {
+
+	return nil
+}
+
+// Test
+type HelloWorldPlugin struct {
+	plugin.MattermostPlugin
+}
+
+func (p *HelloWorldPlugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
+
+// This example demonstrates a plugin that handles HTTP requests which respond by greeting the
+// world.
+func main() {
+	plugin.ClientMain(&HelloWorldPlugin{})
+}
