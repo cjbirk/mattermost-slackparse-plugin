89bd7291b9e7a612797eaec050fe63a78c4b53c6
Charles Birk <charles@cryptolab.net>
2018-09-02 18:10:42 -0400

    Adding logging to plugin for debug

--- plugin.go
+++ plugin.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
+
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
+)
+
+var (
+	Info *log.Logger
 )
 
 // Plugin yadda yadda
@@ -11,15 +19,51 @@ type Plugin struct {
 	Enabled bool
 }
 
+// Init logging anus
+func Init(infoHandle io.Writer) {
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
 // OnActivate is invoked when the plugin is activated.
 func (p *Plugin) OnActivate() error {
-
 	return nil
+}
+
+// FilterPost our post to process via ParseSlackAttachment
+func (p *Plugin) FilterPost(post *model.Post) (*model.Post, string) {
+	Info.Println("THIS IS THE FILTERPOST FUNCTION U DINGUS")
+	if post.Type == model.POST_SLACK_ATTACHMENT {
+		// 	if err := json.NewDecoder(r.Body).Decode(&webhook); err != nil {
+		// 		http.Error(w, err.Error(), http.StatusBadRequest)
+		// 	} else if attachment, err := webhook.SlackAttachment(); err != nil {
+		// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	} else if attachment == nil {
+		// 		return
+		// 	}
+		// post.Props is the field that we're going to parse to get the attach-
+		// ment thing - lamer
+		attachments, _ := post.Props["attachments"].([]*model.SlackAttachment)
+		// do something
+		var newAttachment *model.SlackAttachment
+		newAttachment = attachments[0]
+
+		model.ParseSlackAttachment(post, []*model.SlackAttachment{newAttachment})
+	}
+
+	return post, ""
 }
 
 // MessageWillBePosted is invoked when a message is posted by a user before it is commited
 // to the database.
 func (p *Plugin) MessageWillBePosted(c *plugin.Context, post *model.Post) (*model.Post, string) {
-	model.SlackAttachment()
-	return nil
+	Info.Println("HEY LOOK AT THESE LOGS IN THE MESSAGEWILLBEPOSTED GABBAGE")
+	return p.FilterPost(post)
+}
+
+func main() {
+	Init(os.Stdout)
+	plugin.ClientMain(&Plugin{})
 }
