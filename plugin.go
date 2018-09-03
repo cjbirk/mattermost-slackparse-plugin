69fee2932072d54a1b11b9ea343437e9da6440f6
Charles Birk <charles@cryptolab.net>
2018-09-02 22:48:20 -0400

    Hacking the planet

--- plugin.go
+++ plugin.go
@@ -1,56 +1,42 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
-
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
-)
-
-var (
-	Info *log.Logger
 )
 
 // Plugin yadda yadda
 type Plugin struct {
 	plugin.MattermostPlugin
+	api     plugin.API
 	Enabled bool
 }
 
-// Init logging anus
-func Init(infoHandle io.Writer) {
-
-	Info = log.New(infoHandle,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-}
-
 // OnActivate is invoked when the plugin is activated.
-func (p *Plugin) OnActivate() error {
+func (p *Plugin) OnActivate(api plugin.API) error {
+	p.api = api
+	p.API.LogDebug("I'M JUST A POOR BOY")
+
+	if err := p.OnConfigurationChange(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // FilterPost our post to process via ParseSlackAttachment
 func (p *Plugin) FilterPost(post *model.Post) (*model.Post, string) {
-	Info.Println("THIS IS THE FILTERPOST FUNCTION U DINGUS")
+	p.API.LogDebug("NOBODY LOVES ME")
 	if post.Type == model.POST_SLACK_ATTACHMENT {
-		// 	if err := json.NewDecoder(r.Body).Decode(&webhook); err != nil {
-		// 		http.Error(w, err.Error(), http.StatusBadRequest)
-		// 	} else if attachment, err := webhook.SlackAttachment(); err != nil {
-		// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		// 	} else if attachment == nil {
-		// 		return
-		// 	}
-		// post.Props is the field that we're going to parse to get the attach-
-		// ment thing - lamer
+		p.API.LogDebug("HE'S JUST A POOR BOY")
 		attachments, _ := post.Props["attachments"].([]*model.SlackAttachment)
-		// do something
-		var newAttachment *model.SlackAttachment
-		newAttachment = attachments[0]
-
-		model.ParseSlackAttachment(post, []*model.SlackAttachment{newAttachment})
+		//var newAttachment *model.SlackAttachment
+		//newAttachment = attachments[0]
+		//for attachment := range attachments {
+		//	if attachment
+		//}
+		p.API.LogDebug("FROM A POOR", "FAMILY::::", attachments)
+		model.ParseSlackAttachment(post, []*model.SlackAttachment{attachments})
 	}
 
 	return post, ""
@@ -59,11 +45,10 @@ func (p *Plugin) FilterPost(post *model.Post) (*model.Post, string) {
 // MessageWillBePosted is invoked when a message is posted by a user before it is commited
 // to the database.
 func (p *Plugin) MessageWillBePosted(c *plugin.Context, post *model.Post) (*model.Post, string) {
-	Info.Println("HEY LOOK AT THESE LOGS IN THE MESSAGEWILLBEPOSTED GABBAGE")
+	p.API.LogDebug("SPARE HIM HIS LIFE OF THIS MONSTROSITY")
 	return p.FilterPost(post)
 }
 
 func main() {
-	Init(os.Stdout)
 	plugin.ClientMain(&Plugin{})
 }
